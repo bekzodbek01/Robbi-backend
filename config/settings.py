3ea24397f4008ab65023769305ef2edc7bf1f38b
bekzodbek01 <bekzodbek5201@gmail.com>
2025-05-13 20:09:44 +0500

    1

--- config/settings.py
+++ config/settings.py
@@ -129,11 +129,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+#
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # loyihangizning `static` papkasi
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),  # loyihangizning `static` papkasi
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # yig'ilgan statik fayllar joylashadi
 
 # Media fayllar
