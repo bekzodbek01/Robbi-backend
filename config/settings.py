e49d23ec732629163e264c296f7e2ac31c173769
bekzodbek01 <bekzodbek5201@gmail.com>
2025-06-24 19:20:47 +0500

    docker compos ozgartrildi db ni test qilib keyin boglanadi

--- config/settings.py
+++ config/settings.py
@@ -79,24 +79,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# # # # #
+#
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'robbi_db',
-#         'USER': 'robbi_user',
-#         'PASSWORD': 'robbi_password',
-#         'HOST': 'db_robbi',
-#         'PORT': '5432',  # Mahalliy port (Docker Compose faylidan)..
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# # # # #
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'robbi_db',
+        'USER': 'robbi_user',
+        'PASSWORD': 'robbi_password',
+        'HOST': 'db_robbi',
+        'PORT': '5432',  # Mahalliy port (Docker Compose faylidan)..
+    }
+}
 
 
 # Password validation
